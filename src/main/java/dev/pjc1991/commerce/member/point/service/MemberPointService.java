a12c593f6a5b69f7ec3568c265624d7e8bda9b94
pjc1991 <whdekf@gmail.com>
2023-08-18 21:13:45 +0900

    FEAT : 회원 적립금 서비스 인터페이스 설계

---
+++ src/main/java/dev/pjc1991/commerce/member/point/service/MemberPointService.java
@@ -0,0 +1,40 @@
+package dev.pjc1991.commerce.member.point.service;
+
+import dev.pjc1991.commerce.member.point.domain.MemberPoint;
+import dev.pjc1991.commerce.member.point.dto.MemberPointCreateRequest;
+import dev.pjc1991.commerce.member.point.dto.MemberPointEventSearch;
+import dev.pjc1991.commerce.member.point.dto.MemberPointUseRequest;
+
+import java.util.List;
+
+public interface MemberPointService {
+
+    /**
+     * 회원 적립금 합계 조회
+     * @param memberId
+     * @return
+     */
+    int getMemberPointTotal(int memberId);
+
+    /**
+     * 회원 적립금 적립/사용 내역 조회
+     * @param search
+     * @return
+     */
+    List<MemberPoint> getMemberPointLogList(MemberPointEventSearch search);
+
+    /**
+     * 회원 적립금 적립
+     * @param memberPointCreate
+     * @return
+     */
+    MemberPoint earnMemberPoint(MemberPointCreateRequest memberPointCreate);
+
+    /**
+     * 회원 적립금 사용
+     * @param memberPointUse
+     * @return
+     */
+    MemberPoint useMemberPoint(MemberPointUseRequest memberPointUse);
+
+}
