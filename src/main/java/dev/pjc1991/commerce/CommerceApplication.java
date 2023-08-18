a5f4954fa1a03d34aa1dad83788e4c409f30dbeb
pjc1991 <whdekf@gmail.com>
2023-08-18 11:57:07 +0900

    CHORE : Spring initializr

---
+++ src/main/java/dev/pjc1991/commerce/CommerceApplication.java
@@ -0,0 +1,13 @@
+package dev.pjc1991.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pjc1991/commerce/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pjc1991.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
