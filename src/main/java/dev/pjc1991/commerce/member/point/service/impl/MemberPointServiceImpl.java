4a720dbb7d0a1d9228b8762b868d263032a60bd7
pjc1991 <whdekf@gmail.com>
2023-08-18 21:40:43 +0900

    FEAT : 회원 적립금 서비스 구현체 추가

---
+++ src/main/java/dev/pjc1991/commerce/member/point/service/impl/MemberPointServiceImpl.java
@@ -0,0 +1,37 @@
+package dev.pjc1991.commerce.member.point.service.impl;
+
+import dev.pjc1991.commerce.member.point.domain.MemberPointEvent;
+import dev.pjc1991.commerce.member.point.dto.MemberPointCreateRequest;
+import dev.pjc1991.commerce.member.point.dto.MemberPointEventSearch;
+import dev.pjc1991.commerce.member.point.dto.MemberPointUseRequest;
+import dev.pjc1991.commerce.member.point.service.MemberPointService;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@Slf4j
+public class MemberPointServiceImpl implements MemberPointService {
+    @Override
+    public int getMemberPointTotal(int memberId) {
+        return 0;
+    }
+
+    @Override
+    public List<MemberPointEvent> getMemberPointLogList(MemberPointEventSearch search) {
+        return null;
+    }
+
+    @Override
+    public MemberPointEvent earnMemberPoint(MemberPointCreateRequest memberPointCreate) {
+        return null;
+    }
+
+    @Override
+    public MemberPointEvent useMemberPoint(MemberPointUseRequest memberPointUse) {
+        return null;
+    }
+}
