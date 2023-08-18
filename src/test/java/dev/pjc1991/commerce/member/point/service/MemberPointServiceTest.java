7a284c407bdea2c8a5e05f9adb7d37114052d9b6
pjc1991 <whdekf@gmail.com>
2023-08-18 21:45:40 +0900

    TEST : 회원 적립금 서비스 단위 테스트 추가

--- src/test/java/dev/pjc1991/commerce/member/point/service/MemberPointServiceTest.java
+++ src/test/java/dev/pjc1991/commerce/member/point/service/MemberPointServiceTest.java
@@ -1,12 +1,24 @@
 package dev.pjc1991.commerce.member.point.service;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback(true)
 class MemberPointServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MemberPointServiceTest.class);
+
+    @Autowired
+    MemberPointService memberPointService;
 
     @Test
     void getMemberPointTotal() {
