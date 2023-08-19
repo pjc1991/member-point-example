f4f667460391464488d30189e3616110bdcd9d1f
pjc1991 <whdekf@gmail.com>
2023-08-19 23:27:11 +0900

    FEAT : 사용가능한 회원 적립금 검색 메소드 구현

--- src/main/java/dev/pjc1991/commerce/member/point/dto/MemberPointDetailRemain.java
+++ src/main/java/dev/pjc1991/commerce/member/point/dto/MemberPointDetailRemain.java
@@ -3,6 +3,8 @@ package dev.pjc1991.commerce.member.point.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class MemberPointDetailRemain {
@@ -12,4 +14,7 @@ public class MemberPointDetailRemain {
 
     // 회원 적립금 사용 후 잔액
     private Integer remain;
+
+    // 회원 적립금 적립 시점
+    private LocalDateTime createdAt;
 }
--- src/main/java/dev/pjc1991/commerce/member/point/repository/MemberPointDetailRepositoryCustom.java
+++ src/main/java/dev/pjc1991/commerce/member/point/repository/MemberPointDetailRepositoryCustom.java
@@ -1,10 +1,21 @@
 package dev.pjc1991.commerce.member.point.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.DateTimePath;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPQLQuery;
 import dev.pjc1991.commerce.member.point.domain.MemberPointDetail;
 import dev.pjc1991.commerce.member.point.domain.QMemberPointDetail;
+import dev.pjc1991.commerce.member.point.dto.MemberPointDetailRemain;
+import dev.pjc1991.commerce.member.point.dto.MemberPointDetailSearch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public class MemberPointDetailRepositoryCustom extends QuerydslRepositorySupport {
@@ -46,5 +57,67 @@ public class MemberPointDetailRepositoryCustom extends QuerydslRepositorySupport
         }
 
         return result;
+    }
+
+    /**
+     * 사용할 수 있는 가장 오래된 적립금 상세 내역부터 조회합니다.
+     * @param search
+     * 회원 적립금 상세 내역 조회 파라메터를 담은 오브젝트입니다.
+     * @return
+     * 회원 적립금 상세 내역을 담은 페이지 오브젝트입니다.
+     */
+    public Page<MemberPointDetailRemain> getMemberPointDetailAvailable(MemberPointDetailSearch search) {
+        // 적립금 상세 내역에서 회원 아이디로 조회합니다.
+        // 회원 아이디가 없으면 예외를 발생시킵니다.
+        if (search.getMemberId() == null) {
+            throw new IllegalArgumentException("memberId 가 null 입니다.");
+        }
+
+        QMemberPointDetail memberPointDetail = QMemberPointDetail.memberPointDetail;
+        DateTimePath<LocalDateTime> createdAt = Expressions.dateTimePath(LocalDateTime.class, "createdAt");
+
+        JPQLQuery<MemberPointDetailRemain> query = from(memberPointDetail)
+                .select(Projections.constructor(MemberPointDetailRemain.class,
+                        memberPointDetail.memberPointDetailGroupId,
+                        memberPointDetail.amount.sum().as("remain"),
+                        memberPointDetail.createdAt.min().as(createdAt)
+                ));
+
+        query.where(
+                memberPointDetail.memberPointEvent.memberId.eq(search.getMemberId())
+                , memberPointDetail.memberPointEvent.expireAt.after(LocalDateTime.now())
+        );
+
+        query.groupBy(memberPointDetail.memberPointDetailGroupId);
+        query.orderBy(createdAt.asc());
+        query.limit(search.getSize());
+        query.offset(search.getOffset());
+
+        /*
+         * SELECT
+         *   MEMBER_POINT_DETAIL.MEMBER_POINT_DETAIL_GROUP_ID AS MEMBER_POINT_DETAIL_GROUP_ID,
+         *   SUM(MEMBER_POINT_DETAIL.AMOUNT) AS REMAIN,
+         *   MIN(MEMBER_POINT_DETAIL.CREATED_AT) AS CREATED_AT
+         * FROM
+         *   MEMBER_POINT_DETAIL MEMBER_POINT_DETAIL
+         * INNER JOIN
+         *   MEMBER_POINT_EVENT MEMBER_POINT_EVENT
+         * ON
+         *   MEMBER_POINT_DETAIL.MEMBER_POINT_EVENT_ID = MEMBER_POINT_EVENT.ID
+         * WHERE
+         *   MEMBER_POINT_EVENT.MEMBER_ID = ?
+         * AND
+         *   MEMBER_POINT_EVENT.EXPIRE_AT > ?
+         * GROUP BY
+         *   MEMBER_POINT_DETAIL.MEMBER_POINT_DETAIL_GROUP_ID
+         * ORDER BY
+         *   CREATED_AT ASC
+         * LIMIT ?, ?
+         */
+
+        List<MemberPointDetailRemain> result = query.fetch();
+        long totalCount = query.fetchCount();
+
+        return new PageImpl<MemberPointDetailRemain>(result, PageRequest.of(search.getPage(), search.getSize()), totalCount);
     }
 }
