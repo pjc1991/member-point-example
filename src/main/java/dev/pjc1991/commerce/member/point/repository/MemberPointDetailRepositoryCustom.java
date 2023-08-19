86256eaba4a11b1e074f6e2122ef60354a38b7f3
pjc1991 <whdekf@gmail.com>
2023-08-19 21:30:59 +0900

    FIX : 적립금 총합을 조회할 때 더 이상 그룹 쿼리를 사용하지 않음.
    그룹 쿼리가 필요한 것은 총합 조회가 아니라 사용/만료의 FIFO 처리를 할 때임.

--- src/main/java/dev/pjc1991/commerce/member/point/repository/MemberPointDetailRepositoryCustom.java
+++ src/main/java/dev/pjc1991/commerce/member/point/repository/MemberPointDetailRepositoryCustom.java
@@ -20,15 +20,13 @@ public class MemberPointDetailRepositoryCustom extends QuerydslRepositorySupport
      * @return 회원의 적립금 총합
      */
     public int getMemberPointTotal(int memberId) {
-        // 적립금 총합을 계산하기 위해 회원 상세 내역에서 그룹 ID 기준으로 그룹 쿼리를 실행합니다.
+        // 적립금 총합을 계산하기 위해 적립금 상세 내역에서 회원 아이디로 조회합니다.
         QMemberPointDetail memberPointDetail = QMemberPointDetail.memberPointDetail;
 
         JPQLQuery<Integer> query = from(memberPointDetail)
                 .select(memberPointDetail.amount.sum());
 
         query.where(memberPointDetail.memberPointEvent.memberId.eq(memberId));
-        query.having(memberPointDetail.amount.sum().gt(0));
-        query.groupBy(memberPointDetail.memberPointDetailUseId);
 
         /*
          * SELECT
@@ -39,8 +37,6 @@ public class MemberPointDetailRepositoryCustom extends QuerydslRepositorySupport
          *     MEMBER_POINT_EVENT MEMBER_POINT_EVENT
          * WHERE
          *     MEMBER_POINT_EVENT.MEMBER_ID = ?
-         * GROUP BY
-         *    MEMBER_POINT_DETAIL.MEMBER_POINT_DETAIL_USE_ID
          */
 
         Integer result = query.fetchOne();
