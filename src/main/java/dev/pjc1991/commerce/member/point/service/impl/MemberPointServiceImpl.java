33445a06dc92020c5768dc274168214421a6fb69
pjc1991 <whdekf@gmail.com>
2023-08-20 00:40:22 +0900

    REFACTOR : 적립금 사용 내역 생성 메소드 분리

--- src/main/java/dev/pjc1991/commerce/member/point/service/impl/MemberPointServiceImpl.java
+++ src/main/java/dev/pjc1991/commerce/member/point/service/impl/MemberPointServiceImpl.java
@@ -96,35 +96,42 @@ public class MemberPointServiceImpl implements MemberPointService {
 
         // 잔액이 0이 될 때까지 반복합니다.
         while (useAmountRemain > 0) {
-            // 현재 페이지의 적립금 상세 내역을 조회합니다.
-            Page<MemberPointDetailRemain> memberPointDetailAvailable = memberPointDetailRepositoryCustom.getMemberPointDetailAvailable(search);
-
-            // 현재 페이지의 적립금 상세 내역이 없다면 예외를 발생시킵니다.
-            if (memberPointDetailAvailable.getTotalElements() == 0) {
-                throw new RuntimeException("적립금이 부족합니다.");
-            }
-
-            // 현재 페이지의 적립금 상세 내역을 순회하며 사용하려는 적립금의 잔액을 차감합니다.
-            for (MemberPointDetailRemain memberPointDetailRemain : memberPointDetailAvailable.getContent()) {
-                // 상세 내역의 적립금이 사용 잔액보다 크다면 사용 잔액을 0으로 만들고 반복문을 종료합니다.
-                if (memberPointDetailRemain.getRemain() - useAmountRemain >= 0) {
-                    MemberPointDetail last = MemberPointDetail.useMemberPointDetail(useEvent, memberPointDetailRemain, useAmountRemain);
-                    memberPointDetails.add(last);
-                    useAmountRemain = 0;
-                    break;
-                }
-                // 상세 내역의 적립금보다 사용 잔액이 크다면, 사용 잔액을 상세 내역의 적립금만큼 차감합니다.
-                else {
-                    MemberPointDetail current = MemberPointDetail.useMemberPointDetail(useEvent, memberPointDetailRemain, memberPointDetailRemain.getRemain());
-                    memberPointDetails.add(current);
-                    useAmountRemain -= memberPointDetailRemain.getRemain();
-                }
-
-            }
-
-            search.setPage(search.getPage() + 1);
+            useAmountRemain = clearMemberPointUse(useEvent, search, useAmountRemain, memberPointDetails);
         }
 
         return memberPointDetails;
+    }
+
+    /**
+     * 현재 페이지의 적립금 상세 내역을 조회하고 사용하려는 적립금의 잔액을 차감합니다.
+     * @param useEvent
+     * @param search
+     * @param useAmountRemain
+     * @param memberPointDetails
+     * @return
+     */
+    private int clearMemberPointUse(MemberPointEvent useEvent, MemberPointDetailSearch search, int useAmountRemain, List<MemberPointDetail> memberPointDetails) {
+        // 현재 페이지의 적립금 상세 내역을 조회합니다.
+        Page<MemberPointDetailRemain> memberPointDetailAvailable = memberPointDetailRepositoryCustom.getMemberPointDetailAvailable(search);
+
+        // 현재 페이지의 적립금 상세 내역이 없다면 예외를 발생시킵니다.
+        if (memberPointDetailAvailable.getTotalElements() == 0) {
+            throw new RuntimeException("적립금이 부족합니다.");
+        }
+
+        // 현재 페이지의 적립금 상세 내역을 순회하며 사용하려는 적립금의 잔액을 차감합니다.
+        for (MemberPointDetailRemain memberPointDetailRemain : memberPointDetailAvailable.getContent()) {
+            // 사용 금액과 적립금 상세 내역 중 작은 값으로 생성합니다.
+            int useAmount = Math.min(useAmountRemain, memberPointDetailRemain.getRemain());
+            MemberPointDetail current = MemberPointDetail.useMemberPointDetail(useEvent, memberPointDetailRemain, memberPointDetailRemain.getRemain());
+            memberPointDetails.add(current);
+            useAmountRemain -= memberPointDetailRemain.getRemain();
+
+            // 잔액이 0이 되면 반복을 종료합니다.
+            if (useAmountRemain == 0) break;
+        }
+
+        search.setPage(search.getPage() + 1);
+        return useAmountRemain;
     }
 }
