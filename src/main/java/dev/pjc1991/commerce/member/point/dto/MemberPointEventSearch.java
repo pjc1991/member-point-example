6c97903ee0c2acaa528883414e259f874373602f
pjc1991 <whdekf@gmail.com>
2023-08-19 22:23:22 +0900

    FEAT : 검색용 DTO 구현

--- src/main/java/dev/pjc1991/commerce/member/point/dto/MemberPointEventSearch.java
+++ src/main/java/dev/pjc1991/commerce/member/point/dto/MemberPointEventSearch.java
@@ -1,14 +1,54 @@
 package dev.pjc1991.commerce.member.point.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class MemberPointEventSearch {
+
+    // 회원 아이디
     private int memberId;
-    private int page;
-    private int size;
+    // 페이지 번호 (0부터 시작)
+    private int page = 0;
+    // 페이지 크기
+    private int size = 10;
+    // 조회된 전체 데이터 수
     private int total;
-    private int offset;
+    // 조회 시작 위치
+    private int offset = 0;
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    /**
+     * 페이지 번호나 페이지 크기를 설정하면 조회 시작 위치를 계산합니다.
+     * @param page
+     * 페이지 번호 (0부터 시작)
+     */
+    public void setPage(int page) {
+        this.page = page;
+        calculateOffset();
+    }
+
+    /**
+     * 페이지 번호나 페이지 크기를 설정하면 조회 시작 위치를 계산합니다.
+     * @param size
+     * 페이지 크기
+     */
+    public void setSize(int size) {
+        this.size = size;
+        calculateOffset();
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    /**
+     * 조회 시작 위치를 계산합니다.
+     */
+    private void calculateOffset() {
+        this.offset = page * size;
+    }
+
 }
