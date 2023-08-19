d1ef9f013769abcc42fd8ae08a7a618e75bb3c50
pjc1991 <whdekf@gmail.com>
2023-08-20 00:19:25 +0900

    FEAT : 회원 적립금 사용 요청 DTO 수정

--- src/main/java/dev/pjc1991/commerce/member/point/dto/MemberPointUseRequest.java
+++ src/main/java/dev/pjc1991/commerce/member/point/dto/MemberPointUseRequest.java
@@ -6,6 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MemberPointUseRequest {
-    private int memberId;
-    private int amount;
+    private Integer memberId;
+    private Integer amount;
+
+    public void setAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("사용 금액은 0보다 작을 수 없습니다.");
+        }
+        this.amount = amount;
+    }
 }
