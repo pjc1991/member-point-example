f4ffed73c8faa1234fb9fe35f1fefe19db2b8415
pjc1991 <whdekf@gmail.com>
2023-08-21 14:13:42 +0900

    FEAT : 회원 적립금 합계 조회 API 추가

--- src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
+++ src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
@@ -1,20 +1,30 @@
 package dev.pjc1991.commerce.member.point.controller;
 
+import dev.pjc1991.commerce.member.point.dto.MemberPointTotalResponse;
+import dev.pjc1991.commerce.member.point.service.MemberPointService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberPointController {
 
+    private final MemberPointService memberPointService;
+
     /**
-     * 컨트롤러 테스트용 메소드입니다.
-     * 추후 삭제합니다.
+     * 회원 적립금 합계 조회
+     * @param memberId
+     * 회원 아이디
      * @return
+     * 회원 적립금 합계 응답 오브젝트
+     * memberId: 회원 아이디, totalPoint: 적립금 합계
      */
-    @GetMapping("/hello")
-    public ResponseEntity<?> Hello() {
-        String hello = "Hello";
-        return ResponseEntity.ok(hello);
+    @GetMapping("/member/{memberId}/point/total")
+    public ResponseEntity<?> getMemberPointTotal(@PathVariable int memberId) {
+        MemberPointTotalResponse response = new MemberPointTotalResponse(memberId, memberPointService.getMemberPointTotal(memberId));
+        return ResponseEntity.ok(response);
     }
 }
