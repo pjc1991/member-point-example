7799683761061a57f2adc0ac54b664f1f70b622d
pjc1991 <whdekf@gmail.com>
2023-08-21 14:14:10 +0900

    REFACTOR : ResponseStatus 명시

--- src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
+++ src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
@@ -3,10 +3,12 @@ package dev.pjc1991.commerce.member.point.controller;
 import dev.pjc1991.commerce.member.point.dto.MemberPointTotalResponse;
 import dev.pjc1991.commerce.member.point.service.MemberPointService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +25,7 @@ public class MemberPointController {
      * memberId: 회원 아이디, totalPoint: 적립금 합계
      */
     @GetMapping("/member/{memberId}/point/total")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> getMemberPointTotal(@PathVariable int memberId) {
         MemberPointTotalResponse response = new MemberPointTotalResponse(memberId, memberPointService.getMemberPointTotal(memberId));
         return ResponseEntity.ok(response);
