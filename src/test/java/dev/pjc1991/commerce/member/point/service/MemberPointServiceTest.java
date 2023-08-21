19f01f7038d4ead9e442c7b2e4d164f1e0544abc
pjc1991 <whdekf@gmail.com>
2023-08-22 01:44:39 +0900

    CHORE : 불필요한 import 삭제

--- src/test/java/dev/pjc1991/commerce/member/point/service/MemberPointServiceTest.java
+++ src/test/java/dev/pjc1991/commerce/member/point/service/MemberPointServiceTest.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.util.StopWatch;
 
 import static org.junit.jupiter.api.Assertions.*;
