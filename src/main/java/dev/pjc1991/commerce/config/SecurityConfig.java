0e9c2a1acc265f41dce1c392e728b731fbcf03d9
pjc1991 <whdekf@gmail.com>
2023-08-21 17:43:10 +0900

    FIX : /h2-console 로 접근할 수 있도록 localhost 에서 iframe 허가

--- src/main/java/dev/pjc1991/commerce/config/SecurityConfig.java
+++ src/main/java/dev/pjc1991/commerce/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,7 @@ public class SecurityConfig {
         return http
                 .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .build();
     }
 }
