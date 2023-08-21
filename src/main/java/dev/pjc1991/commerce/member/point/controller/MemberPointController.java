33a04c7b2451917cd2a9c48220d690509ba1a3ac
pjc1991 <whdekf@gmail.com>
2023-08-21 15:31:03 +0900

    CHORE : import 최적화

--- src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
+++ src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
@@ -11,10 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
