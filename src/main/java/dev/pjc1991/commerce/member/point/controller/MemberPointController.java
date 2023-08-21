347b5f7029ebfb65f4a3730349be3a737c9aa042
pjc1991 <whdekf@gmail.com>
2023-08-21 14:19:26 +0900

    FEAT : 회원 적립금 적립/사용 내역 조회 API 추가

--- src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
+++ src/main/java/dev/pjc1991/commerce/member/point/controller/MemberPointController.java
@@ -1,8 +1,11 @@
 package dev.pjc1991.commerce.member.point.controller;
 
+import dev.pjc1991.commerce.member.point.domain.MemberPointEvent;
+import dev.pjc1991.commerce.member.point.dto.MemberPointEventSearch;
 import dev.pjc1991.commerce.member.point.dto.MemberPointTotalResponse;
 import dev.pjc1991.commerce.member.point.service.MemberPointService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -29,5 +32,22 @@ public class MemberPointController {
     public ResponseEntity<?> getMemberPointTotal(@PathVariable int memberId) {
         MemberPointTotalResponse response = new MemberPointTotalResponse(memberId, memberPointService.getMemberPointTotal(memberId));
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 회원 적립금 적립/사용 내역 조회
+     * @param memberId
+     * 회원 아이디
+     * @param search
+     * 검색 조건
+     * page : 페이지 번호, size : 페이지 사이즈
+     * @return
+     */
+    @GetMapping("/member/{memberId}/point")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> getMemberPointEvents(@PathVariable int memberId, MemberPointEventSearch search) {
+        search.setMemberId(memberId);
+        Page<MemberPointEvent> memberPointEvents = memberPointService.getMemberPointEvents(search);
+        return ResponseEntity.ok(memberPointEvents);
     }
 }
