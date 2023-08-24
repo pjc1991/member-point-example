ae849900dd2ea5b788b33d0e4b0d26f222863769
pjc1991 <whdekf@gmail.com>
2023-08-24 19:44:03 +0900

    FEAT : Docker 를 이용한 실행 추가

---
+++ src/main/java/dev/pjc1991/commerce/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package dev.pjc1991.commerce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@Profile("postgres")
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
